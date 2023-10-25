e0c1d84c44f0f16f43686c24400a9a6dd2b87e89
LucasCoria12 <lucaslcoria122@gmail.com>
2023-10-25 14:20:22 -0300

    .UserDetailsService

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -6,16 +6,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import service.UserService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private UserService userService;
+
     public SecurityConfig() {
+    }
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.userDetailsService(null); // que pija va aca? en ves de null
+        http.userDetailsService(userService); // que pija va aca? en ves de null
 
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/webjars/**", "/resources/**", "/css/**").permitAll()
