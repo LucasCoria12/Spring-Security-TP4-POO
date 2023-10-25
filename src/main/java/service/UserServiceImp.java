6e19ca9bfcaed572508d08c305459e859c5d1316
LucasCoria12 <lucaslcoria122@gmail.com>
2023-10-25 11:10:45 -0300

    "se agregaron comentarios"

--- src/main/java/service/UserServiceImp.java
+++ src/main/java/service/UserServiceImp.java
@@ -1,6 +1,5 @@
 package service;
 
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,16 +9,16 @@ import model.User;
 
 @Service
 public class UserServiceImp implements UserService {
-    
+
     @Override
-    public User create(User user){ 
-            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-            return user;  
+    public User create(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return user;
     }
-    
+
     @Override
-    public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException{
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null; // anda a saber que se retorna aca
     }
 
 }
